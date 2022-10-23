f6aa5b2fc71271ab76326be74fae4a330b5a22be
joscord <josecorderodev@gmail.com>
2022-10-23 12:08:53 -0300

    feat: define createProductCard and showProducts functions

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
+fetchBtn = document.getElementById('fetch-btn');
+
 const fetchNotebooks = async () => {
 	const response = await axios.get(
 		'https://api.mercadolibre.com/sites/MLC/search?q=notebook'
 	);
 	const { results: notebooks } = response.data;
+	console.log(notebooks[0]);
 	return notebooks;
 };
 
 const createProductCard = product => {
 	const { title, price, thumbnail } = product;
-	const card = document.createElementById('div');
+	const card = document.createElement('div');
 	card.className = 'card';
 	card.style.width = '18rem';
-	card.innerHtml = `
+	card.innerHTML = `
     <img src="${thumbnail}" class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title">${title}</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <p class="card-text">lorem ipsum</p>
     </div>
     <div class="card-body">
         <a href="#" class="card-link">${price}</a>
@@ -24,3 +27,13 @@ const createProductCard = product => {
     `;
 	return card;
 };
+
+const showProducts = async () => {
+	const contentDiv = document.getElementById('content');
+	const products = await fetchNotebooks();
+	products.map(product => {
+		contentDiv.appendChild(createProductCard(product));
+	});
+};
+
+fetchBtn.addEventListener('click', showProducts);
