e3242e851537342158db0449110962f6dd051a5c
joscord <josecorderodev@gmail.com>
2022-10-23 11:53:26 -0300

    feat: define createProductCard

--- js/script.js
+++ js/script.js
@@ -5,3 +5,22 @@ const fetchNotebooks = async () => {
 	const { results: notebooks } = response.data;
 	return notebooks;
 };
+
+const createProductCard = product => {
+	const { title, price, thumbnail } = product;
+	const card = document.createElementById('div');
+	card.className = 'card';
+	card.style.width = '18rem';
+	card.innerHtml = `
+    <img src="${thumbnail}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+    <div class="card-body">
+        <a href="#" class="card-link">${price}</a>
+        <a href="#" class="card-link">Buy Now</a>
+    </div>
+    `;
+	return card;
+};
